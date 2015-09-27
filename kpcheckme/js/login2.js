76289def44357bb4f157ab89fe093593986381fe
Sean Lingren <srlingren@gmail.com>
2015-09-26 17:03:09 -0700

    Completely new portfolio website. Also hosting our KP CheckMe app here.

---
+++ kpcheckme/js/login2.js
@@ -0,0 +1,9 @@
+ $("#login-button").click(function(event){
+		 event.preventDefault();
+	 
+	 $('form').fadeOut(500);
+	 $('.wrapper').addClass('form-success');
+	 window.location.href = "dashboard.html";
+
+});
+
